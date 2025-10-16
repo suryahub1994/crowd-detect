8937359b9899a15d11dde602e1c271122e9bfa06
Suryasis Paul <146093678+suryahub1994@users.noreply.github.com>
2025-10-16 13:13:29 +0530

    Update algo_factory.py

--- algo_factory.py
+++ algo_factory.py
@@ -1,7 +1,10 @@
 from config import *
 from kmeans import *
+from db_scan import *
 
 
 def getcentroidalgorithm():
     if ALGORITHM == "KMEANS":
         return KMeansAlgo()
+    if ALGORITHM == "DBSCAN":
+        return DBSCANAlgo()
