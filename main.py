6c7287c70e432234827c4e9262355518d34d3ddc
Suryasis Paul <146093678+suryahub1994@users.noreply.github.com>
2025-10-16 13:16:03 +0530

    Update main.py -> Code for main.py for consumption

--- main.py
+++ main.py
@@ -47,17 +47,25 @@ while cap.isOpened():
         list_of_boxes.append(bbox)
     centroid_algorithm = getcentroidalgorithm()
     centroid_and_boxes_list = centroid_algorithm.getcentroids(list_of_boxes)
+    if (VISUALIZE_BOXES):
+        for box in centroid_and_boxes_list:
+            if(box.centroid_class == -1):
+                color = (0, 0, 0)
+            else:
+                color = COLORMAP_30[box.centroid_class % len(COLORMAP_30)]
+            cv2.circle(frame, (box.x_centroid, box.y_centroid), 4, color, -1)
     centroid_aggregator = {}
-    for i in range(NO_OF_CLUSTERS):
+    for i in range(centroid_algorithm.getnoofcentroids()):
         centroid_aggregator[i] = {"min_x": 5000, "min_y": 5000, "max_x": -5000, "max_y": -5000 , "centroid_x": -1, "centroid_y":-1, "count": 0, "label":i}
     for bbox in centroid_and_boxes_list:
-        centroid_aggregator[bbox.centroid_class]["count"]+=1
-        centroid_aggregator[bbox.centroid_class]["min_x"] =  min(centroid_aggregator[bbox.centroid_class]["min_x"], bbox.x_left_top)
-        centroid_aggregator[bbox.centroid_class]["min_y"] =  min(centroid_aggregator[bbox.centroid_class]["min_y"], bbox.y_left_top)
-        centroid_aggregator[bbox.centroid_class]["max_x"] = max(centroid_aggregator[bbox.centroid_class]["max_x"], bbox.x_right_bottom)
-        centroid_aggregator[bbox.centroid_class]["max_y"] = max(centroid_aggregator[bbox.centroid_class]["max_y"], bbox.y_right_bottom)
-        centroid_aggregator[bbox.centroid_class]["centroid_x"] = bbox.x_centroid
-        centroid_aggregator[bbox.centroid_class]["centroid_y"] = bbox.y_centroid
+        if bbox.centroid_class != -1:
+            centroid_aggregator[bbox.centroid_class]["count"]+=1
+            centroid_aggregator[bbox.centroid_class]["min_x"] =  min(centroid_aggregator[bbox.centroid_class]["min_x"], bbox.x_left_top)
+            centroid_aggregator[bbox.centroid_class]["min_y"] =  min(centroid_aggregator[bbox.centroid_class]["min_y"], bbox.y_left_top)
+            centroid_aggregator[bbox.centroid_class]["max_x"] = max(centroid_aggregator[bbox.centroid_class]["max_x"], bbox.x_right_bottom)
+            centroid_aggregator[bbox.centroid_class]["max_y"] = max(centroid_aggregator[bbox.centroid_class]["max_y"], bbox.y_right_bottom)
+            centroid_aggregator[bbox.centroid_class]["centroid_x"] = bbox.x_centroid
+            centroid_aggregator[bbox.centroid_class]["centroid_y"] = bbox.y_centroid
     for label, values in centroid_aggregator.items():
         xlefttop = int(values["min_x"])
         ylefttop = int(values["min_y"])
@@ -71,12 +79,12 @@ while cap.isOpened():
         radius = max(radius_x, radius_y)
         radius = int(radius)
         intensity = min(1.0, values["count"] / 10.0)
-        cv2.circle(heatmap, (x_centroid, y_centroid), radius, intensity, -1)
+        if xlefttop != 5000:
+            cv2.circle(heatmap, (x_centroid, y_centroid), radius, intensity, -1)
     heatmap = cv2.GaussianBlur(heatmap, (91, 91), 0)
     heatmap = cv2.normalize(heatmap, None, 0, 255, cv2.NORM_MINMAX)
     colored = cv2.applyColorMap((heatmap).astype(np.uint8), cv2.COLORMAP_TURBO)
     overlay = cv2.addWeighted(frame, 0.5, colored, 0.5, 0)
-    #cv2.imwrite(f"dump/{count}.jpg", overlay)
     out.write(overlay)
     count += 1
 
